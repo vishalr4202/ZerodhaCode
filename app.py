f9b3ab95e28e142e09b20acdd1914a79f9aaf99c
vishalr4202 <vishalr4202@gmail.com>
2022-02-06 23:28:06 +0530

    2/2/2

--- app.py
+++ app.py
@@ -48,7 +48,7 @@ def hello_world():
     # os.system('start chrome ' + url)
 
     # webbrowser.open(url)
-    return "<p>Hello, Nifty 30 nov!</p>"
+    return "<p>Hello, Nifty 6 Feb!</p>"
 
 
 @app.route("/welcome")
