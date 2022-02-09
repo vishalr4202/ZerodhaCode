23544b7d821c36bb2be3d39606b891b9fee1fd8d
vishalr4202 <vishalr4202@gmail.com>
2022-02-09 09:09:25 +0530

    hi 9th feb

--- kitesettings.py
+++ kitesettings.py
@@ -2,4 +2,4 @@
 API_KEY = "2nqrb7e20f9gy2mp"
 api_secret = "24hppoib99gudt5t4glq7yw5m9dr29ax"
 # ====================================================
-access_token = "GSzV0EA2hdw0ptoCqwz2Cn3Qpz7kBlRI"
+access_token = "ja1210c3zVeF6HgDqGJh8b4HwETYJ6Lr"
